284f9d1e873bcf0be552dc18adae1f7daa030645
Jonathan Cutrell <jonathan@whiteboard.is>
2015-09-25 09:17:36 -0400

    changes

--- wp-content/themes/launchframe/gulpfile.js
+++ wp-content/themes/launchframe/gulpfile.js
@@ -2,17 +2,29 @@
 ////
 //
 
+
+
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps'),
-	watch = require('gulp-watch'),
-	gulpLoadPlugins = require("gulp-load-plugins"),
-	tasks = gulpLoadPlugins({scope: ["devDependencies"]}),
-	gutil = require('gulp-util'),
-	livereload = require('gulp-livereload'),
-	notify = tasks.notify;
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  scsslint = require('gulp-scss-lint'),
+  watch = require('gulp-watch'),
+  gulpLoadPlugins = require("gulp-load-plugins"),
+  tasks = gulpLoadPlugins({scope: ["devDependencies"]}),
+  gutil = require('gulp-util'),
+  livereload = require('gulp-livereload'),
+  cssGlobbing = require('gulp-css-globbing'),
+  babel = require('gulp-babel'),
+  eslint = require('gulp-eslint'),
+  replace = require('gulp-replace'),
+  del = require('del'),
+  notify = tasks.notify;
 
 gulp.task('dist-sass', function () {
+    gulp.src('./assets/src/scss/*.scss')
+      .pipe(scsslint({
+        'config': 'scss-lint.yml',
+      }));
     gulp.src('./assets/src/scss/application.scss')
 	  .pipe(tasks.sourcemaps.init())
 	    .pipe(tasks.sass())
@@ -27,11 +39,23 @@ gulp.task('dist-sass', function () {
 gulp.task('dist-js', function () {
 
 	gulp.src('./assets/src/js/script.js')
+      .pipe(sourcemaps.init())
       .pipe(tasks.concat('./assets/dist/js/script.js'))
+      .pipe(eslint())
+      .pipe(babel())
       .pipe(gulp.dest('./'))
       .pipe(tasks.rename({suffix: '.min'}))
       .pipe(tasks.uglify())
-      .pipe(gulp.dest('./'));
+      .pipe(eslint.format())
+      .pipe(gulp.dest('./'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(livereload())
+      .on('error', function(message){
+        console.log(message);
+      });
+	gulp.src('./assets/vendor/slick.js/slick/slick.min.js')
+    .pipe(tasks.concat('./assets/dist/js/plugins.js'))
+    .pipe(gulp.dest('./'))
 
 });
 
