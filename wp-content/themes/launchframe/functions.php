c39a1224078cab8ed73d2418e2d86fc8bb3b1cf8
patrick <patrick@whiteboard.is>
2016-05-27 09:55:10 -0400

    Removing 'plugin' script dependancy from application enqueue_script

--- wp-content/themes/launchframe/functions.php
+++ wp-content/themes/launchframe/functions.php
@@ -47,7 +47,7 @@ class LaunchframeSite extends TimberSite {
   function register_scripts() {
   	global $package_version;
     //wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js', false, $package_version, true );
-    //wp_enqueue_script( 'application', get_template_directory_uri() . '/assets/dist/js/script.js', array('jquery', 'plugins'), $package_version, true );
+    //wp_enqueue_script( 'application', get_template_directory_uri() . '/assets/dist/js/script.js', array('jquery'), $package_version, true );
   }
 
 	function register_post_types() {
