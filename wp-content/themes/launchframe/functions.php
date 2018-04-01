504caed96d04065611b6d8f69f37537d67a0b8ed
Peter Court <peter@petercourt.com>
2018-04-01 12:52:44 -0400

    Remove deregister jQuery script

--- wp-content/themes/launchframe/functions.php
+++ wp-content/themes/launchframe/functions.php
@@ -26,7 +26,7 @@ class LaunchframeSite extends TimberSite {
 		parent::__construct();
 	}
 	function lf_cleanup() {
-	wp_deregister_script('jquery');
+	// wp_deregister_script('jquery');
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
