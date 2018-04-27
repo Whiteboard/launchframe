24427d1b30445e66d45d308edc5d4e42bb2e1b00
Jonathan Cutrell <jonathan@whiteboard.is>
2018-04-27 16:02:57 -0400

    changing base test file

--- wp-content/themes/launchframe/.lf/assets/src/js/test.js
+++ wp-content/themes/launchframe/.lf/assets/src/js/test.js
@@ -1,4 +1,4 @@
-const __FRAME__ = require('../frames/__FRAME__');
+import {default as __FRAME__} from '../frames/__FRAME__';
 
 test('Test init for __FRAME__', () => {
 	xit("Write a test for __FRAME__");
