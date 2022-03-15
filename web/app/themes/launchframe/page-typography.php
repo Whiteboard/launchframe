b97d6bc7acbf296304c49598c71f42d900320d67
Sarah Tebo <sarahtebo@gmail.com>
2022-03-15 14:27:06 -0400

    Added ‘typography’ page template we use for creating styles

---
+++ web/app/themes/launchframe/page-typography.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Template Name: Typography
+ */
+
+namespace App;
+
+use App\Http\Controllers\Controller;
+use Rareloop\Lumberjack\Http\Responses\TimberResponse;
+use Rareloop\Lumberjack\Page;
+use Timber\Timber;
+
+class PageTypographyController extends Controller
+{
+    public function handle()
+    {
+        $context = Timber::get_context();
+        $page = new Page();
+
+        $context['page'] = $page;
+        $context['title'] = $page->title;
+
+        return new TimberResponse('templates/page-typography.twig', $context);
+    }
+}
